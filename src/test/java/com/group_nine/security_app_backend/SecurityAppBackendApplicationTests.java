e64dfe509d0e38b8c20bc684c7c9a85c4a538509
SprightBlue <rgutierrez649@alumno.unlam.edu.ar>
2026-04-26 20:05:08 -0300

    Initial commit

---
+++ src/test/java/com/group_nine/security_app_backend/SecurityAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group_nine.security_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
